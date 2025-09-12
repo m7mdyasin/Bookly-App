30f56e21a2013904086a1ee985158ed679a1ef6d
mohammedyasin021 <160766863+mohammedyasin021@users.noreply.github.com>
2025-09-12 20:42:09 +0300

    create home repos methods

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/core/errors/faliure.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Faliure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Faliure, List<BookModel>>> fetchFeaturedBooks();
+}
